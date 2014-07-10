c44dfbac4a2583be1fe0f8db24c17040d05b7522
Paul Serraino <pserraino99@gmail.com>
2014-07-09 22:13:47 -0500

    responsive nav

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function ($, window) {
+	$(main);
+
+	function main () {
+		responsiveMenu();
+	}
+
+	function responsiveMenu() {
+		var $win = $(window);
+
+		$('.menu-icon').on('click', function (evt) {
+			$('.nav').slideToggle();
+			$(this).toggleClass('hamburger');
+			$(this).toggleClass('x-mark rotate');
+		});
+
+		$win.on("resize", function (evt) {
+			if ($win > 570)
+				$('.nav').removeAttr("style");
+		});
+	}
+
+})(jQuery, window);
